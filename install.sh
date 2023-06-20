f728cefc68686f1b659765921c57465532816362
Davy39 <davy@ik.me>
2023-06-20 22:33:41 +0200

    Modification de l'installateur

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/bash 
+# Installation de la version précédente de wkhtmltopdf
 sudo apt remove wkhtmltopdf
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 sudo dpkg -i wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 rm wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+
+# Installation des dépendances
 sudo apt-get install python3-pyqt5 python3-pdfkit
+
+# Téléchargement de l'application et des ressources
 sudo mkdir -p /opt/cuisine
 sudo cp logo.svg script.py icon.png /opt/cuisine/
+sudo wget https://raw.githubusercontent.com/Lycee-Experimental/cuisine-LXP/main/logo.svg -O /opt/cuisine/logo.svg
+sudo wget https://raw.githubusercontent.com/Lycee-Experimental/cuisine-LXP/main/icon.png -O /opt/cuisine/icon.png
+sudo chmod 644 /opt/cuisine/icon.png
+sudo wget https://raw.githubusercontent.com/Lycee-Experimental/cuisine-LXP/main/script.py -O /opt/cuisine/script.py
+sudo chmod +x /opt/cuisine/script.py
 
+# Génération d'un élément de menu
 sudo echo """[Desktop Entry]
 Encoding=UTF-8
 Version=1.0
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Cuisine-LXP
+
+Une application pour générer les pièces comptables de la cuisine du Lycée Expérimental
+
+Pour installer l'application sur un OS Linux dérivé de Debian (Debian, Ubuntu, Mint...), lancer la commande suivante dans un terminal (CTRL+ALT+T) :
+
+```bash
+wget -q -O - "https://raw.githubusercontent.com/Lycee-Experimental/cuisine-LXP/main/install.sh" | bash
+```
